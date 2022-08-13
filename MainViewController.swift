45cbc694cc6fe73ee6d50775811c605c4ccbc403
AlexShi <alexshihov55@gmail.com>
2022-08-13 21:11:31 +0300

    Init

---
+++ MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  Look!
+//
+//  Created by Александр on 13.08.2022.
+//  Copyright © 2022 Александр. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
